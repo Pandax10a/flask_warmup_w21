a642a90dc2212a72a16eb73d4ec6ca309cbcd0f4
panda1 <blueblue0011@gmail.com>
2022-10-04 16:47:24 -0700

    updated check

--- app.py
+++ app.py
@@ -1,37 +1,41 @@
 # import modules to make it work
-from unittest import result
+
 from flask import Flask
 import dbhelpers as dh
 import json
 
 app = Flask(__name__)
 
-@app.get('/books')
+@app.get('/api/books')
 
 def all_books():
     result=dh.run_statement('CALL return_all_books_author()')
-    book_json = json.dumps(result, default=str)
-    return book_json
+    if(type(result) == list):
+        book_json = json.dumps(result, default=str)
+        return book_json
 
-@app.get('/books_authored')
+@app.get('/api/books_authored')
 
 def book_count():
     result=dh.run_statement('CALL author_count_of_books')
-    book_json = json.dumps(result, default=str)
-    return book_json
+    if(type(result) == list):
+        book_json = json.dumps(result, default=str)
+        return book_json
 
-@app.get('/best_selling_book')
+@app.get('/api/best_selling_book')
 
 def best_selling():
     result=dh.run_statement('CALL most_copy_sold()')
-    book_json = json.dumps(result, default=str)
-    return book_json
-@app.get('/best_selling_author')
+    if(type(result) == list):
+        book_json = json.dumps(result, default=str)
+        return book_json
+@app.get('/api/best_selling_author')
 
 def best_selling_author():
     result=dh.run_statement('CALL most_copies_sold')
-    book_json = json.dumps(result, default=str)
-    return book_json
+    if(type(result) == list):
+        book_json = json.dumps(result, default=str)
+        return book_json
 
 
 app.run(debug=True)
